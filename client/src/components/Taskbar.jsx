bfa8c700a7b045518608f1b87c050cc6d1f75e1d
mattmuroya <matt.muroya@gmail.com>
2022-05-30 16:32:02 -1000

    replace desktop icons/logos

--- client/src/components/Taskbar.jsx
+++ client/src/components/Taskbar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Context } from "../contexts/Context";
 
-import windowsLogo from "../assets/images/windows-logo.png";
+import windowsLogo from "../assets/images/windows.png";
 
 export default function Taskbar() {
   const { user } = useContext(Context);
